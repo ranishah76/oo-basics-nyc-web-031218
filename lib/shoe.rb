900d038aecf19683041f851779d99ac13f135529
Rani Shah <rani76@gmail.com>
2018-03-14 22:42:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,10 +4,7 @@ attr_accessor :brand, :color, :size, :material, :condiiton
 
 def initialize(brand)
   @brand = brand
-  @color = color
-  @size = size
-  @material = material
-  @condition = condition
+
 end
 
 end
