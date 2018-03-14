330287f17a9d4a91ca3f55be73e6ebfcc730d5b4
Rani Shah <rani76@gmail.com>
2018-03-14 22:45:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
 class Shoe
 
-attr_accessor :brand, :color, :size, :material, :condiiton
+attr_accessor :brand, :color, :size, :material,
+attr_reader :condiiton
 
 def initialize(brand)
   @brand = brand
 end
+
+def initialize(condition = "tattered")
+  @condition = condition
+end
+
 
 end
